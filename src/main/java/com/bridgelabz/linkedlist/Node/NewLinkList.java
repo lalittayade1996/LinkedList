9cac6224abed3ac217a7964c4152a43a6dc7b8aa
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-06-27 20:46:58 +0530

    Added : Linked List UC9

--- src/main/java/com/bridgelabz/linkedlist/Node/NewLinkList.java
+++ src/main/java/com/bridgelabz/linkedlist/Node/NewLinkList.java
@@ -78,6 +78,27 @@ public class NewLinkList<K> {
 		}
 	}
 
+	public void searchAndDelete(int num) {
+		INode tempNode = head;
+		while (tempNode.getNext() != null) {
+			if (tempNode.getNext().getKey().equals(num)) {
+				INode temp = tempNode.getNext().getNext();
+				tempNode.setNext(temp);
+			}
+			tempNode = tempNode.getNext();
+		}
+	}
+
+	public int size() {
+		INode temp = head;
+		int count = 0;
+		while (temp != null) {
+			count++;
+			temp = temp.getNext();
+		}
+		return count;
+	}
+
 	public void printNodes() {
 		StringBuffer nodes = new StringBuffer("My Nodes: ");
 		INode tempNode = head;
--- src/test/java/com/bridgelabz/linkedlist/Node/NodeTest.java
+++ src/test/java/com/bridgelabz/linkedlist/Node/NodeTest.java
@@ -56,6 +56,7 @@ public class NodeTest {
 		linkedList.add(firstNode);
 		linkedList.append(thirdNode);
 		linkedList.insert(firstNode, secondNode);
+		linkedList.printNodes();
 		boolean res = linkedList.head.equals(firstNode) && linkedList.head.getNext().equals(secondNode)
 				&& linkedList.tail.equals(thirdNode);
 		Assert.assertTrue(res);
@@ -71,6 +72,7 @@ public class NodeTest {
 		linkedList.append(secondNode);
 		linkedList.append(thirdNode);
 		linkedList.pop();
+		linkedList.printNodes();
 		boolean res = linkedList.head.equals(secondNode) && linkedList.head.getNext().equals(thirdNode);
 		Assert.assertTrue(res);
 	}
@@ -85,6 +87,7 @@ public class NodeTest {
 		linkedList.append(secondNode);
 		linkedList.append(thirdNode);
 		linkedList.popLast();
+		linkedList.printNodes();
 		boolean res = linkedList.head.equals(firstNode) && linkedList.head.getNext().equals(secondNode);
 		Assert.assertTrue(res);
 	}
@@ -99,6 +102,7 @@ public class NodeTest {
 		linkedList.append(secondNode);
 		linkedList.append(thirdNode);
 		boolean res = linkedList.search(30);
+		linkedList.printNodes();
 		Assert.assertTrue(res);
 	}
 
@@ -113,7 +117,26 @@ public class NodeTest {
 		linkedList.append(secondNode);
 		linkedList.append(thirdNode);
 		linkedList.searchAndInsert(30, newNode);
+		linkedList.printNodes();
 		boolean res = linkedList.head.getNext().equals(secondNode) && secondNode.getNext().equals(newNode);
+		Assert.assertTrue(res);
+	}
+
+	@Test
+	public void Given3Numbers_WhenSearch_40And_DeleteAnd_DisplaySize_OfLinkedList() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		Node<Integer> newNode = new Node<Integer>(40);
+		NewLinkList linkedList = new NewLinkList();
+		linkedList.add(firstNode);
+		linkedList.append(secondNode);
+		linkedList.append(thirdNode);
+		linkedList.searchAndInsert(30, newNode);
+		linkedList.searchAndDelete(40);
+		linkedList.printNodes();
+		System.out.println("Size of a Linked List" + linkedList.size());
+		boolean res = linkedList.head.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
 		Assert.assertTrue(res);
 	}
 }
